afadfca163edbb007d76438e91be5d9f2b5f2b21
Pawpawzz <lepioupioutueur@gmail.com>
2024-05-31 17:23:51 +0200

    feat: added env var in environment.ts

--- UniRide/src/environements/environement.ts
+++ UniRide/src/environements/environement.ts
@@ -1,7 +1,10 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  backUrl: 'https://127.0.0.1:5050', // URL de développement
-  frontUrl: 'https://localhost:4200',
-  googleKey: 'AIzaSyBMreuA5LC2BJ2f-HFPPhYISSIu0mSS2Gs'
+  backUrl: '${ENV_BACKEND_URI}', // URL de développement
+  // backUrl: 'https://127.0.0.1:5050', // URL de développement
+  frontUrl: '${FRONT_URI}',
+  // frontUrl: 'https://localhost:4200',
+  googleKey: '${GOOGLE_API_KEY}'
+  // googleKey: 'AIzaSyBMreuA5LC2BJ2f-HFPPhYISSIu0mSS2Gs'
 };
