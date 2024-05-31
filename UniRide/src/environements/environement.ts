82034903065dbfa9b8784db1d5d19b4edbda1178
Grégoire <95708040+Pawpawzz@users.noreply.github.com>
2024-05-31 18:36:28 +0200

    Update environement.ts

--- UniRide/src/environements/environement.ts
+++ UniRide/src/environements/environement.ts
@@ -2,9 +2,6 @@
 export const environment = {
   production: false,
   backUrl: '${ENV_BACKEND_URI}', // URL de développement
-  // backUrl: 'https://127.0.0.1:5050', // URL de développement
   frontUrl: '${FRONT_URI}',
-  // frontUrl: 'https://localhost:4200',
   googleKey: '${GOOGLE_API_KEY}'
-  // googleKey: 'AIzaSyBMreuA5LC2BJ2f-HFPPhYISSIu0mSS2Gs'
 };
